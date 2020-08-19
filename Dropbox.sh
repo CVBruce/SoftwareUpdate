e7011a9eec05f44eba82179f7a3d0219004009ef
CVBruce <CVBruce@users.noreply.github.com>
2020-08-18 17:11:17 -0700

    Update Dropbox.sh
    pretty print

--- Dropbox.sh
+++ Dropbox.sh
@@ -7,32 +7,32 @@ DRIVE=`basename $0 .sh`
 # Host name is used as the directory on the cloud drive that will be mounted on the local mount point.
 HOSTNAME=`uname -n`
 #
-#
-# 1. Check for executable existance of rclone
-#
+#---------------------------------------------------
+# Check for executable existance of rclone
+#---------------------------------------------------
 if [ ! -e `which rclone` ]
 then 
 	echo "---> rclone not found.";
 	exit 4;
 fi
-#
-# 2. Check for readable existance of rclone.conf
-#
+#-------------------------------------------------
+# Check for readable existance of rclone.conf
+#-------------------------------------------------
 if [ ! -r $HOME/.config/rclone/rclone.conf ]
 then
 	echo "---> rclone configuration file not found.";
 	exit 4;
 fi
-#
-# 3. Check for existance of mount point.
-#
+#--------------------------------------------------
+# Check for existance of mount point.
+#--------------------------------------------------
 if [ ! -d $HOME/$DRIVE ]
 then
 	echo "---> rclone mount point not found.";
 	exit 4;
 fi
-#
-# 4. mount the remote directory.
-#
+#-------------------------------------------------
+# Mount the remote directory.
+#-------------------------------------------------
 rclone mount $DRIVE:/$HOSTNAME $HOME/$DRIVE --daemon --allow-non-empty; 
 exit $?;
